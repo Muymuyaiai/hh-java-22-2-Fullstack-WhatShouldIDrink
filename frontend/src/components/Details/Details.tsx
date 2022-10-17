e91404c7656b22c72b17c06234fe835b3e9080d8
leonmaar <myu@gmx.net>
2022-10-17 17:12:33 +0200

    added suggested drink

--- frontend/src/components/Details/Details.tsx
+++ frontend/src/components/Details/Details.tsx
@@ -6,7 +6,19 @@ type DetailProps ={
     drink : Drink;
 }
 
+
+
 export default function Details(props:DetailProps){
+
+
+    function toIngLst (drink: Drink){
+        let newIngLst = [];
+        for (let i = 0; i<15; i++){
+            !Object.values(drink)[i+6] || newIngLst.push(Object.values(drink)[i+6] + " : " + Object.values(drink)[i+21])
+        }
+        return newIngLst;
+    }
+
 
 
     return(
@@ -21,21 +33,7 @@ export default function Details(props:DetailProps){
             <p>Category:{" "+props.drink.strCategory}</p>
                  <h3>Ingredients</h3>
                  <ul>
-                     {props.drink.strIngredient1 ? <li>{props.drink.strIngredient1 + " = " + props.drink.strMeasure1}</li>:''}
-                     {props.drink.strIngredient2 ? <li>{props.drink.strIngredient2 + " = " + props.drink.strMeasure2}</li>:''}
-                     {props.drink.strIngredient3 ? <li>{props.drink.strIngredient3 + " = " + props.drink.strMeasure3}</li>:''}
-                     {props.drink.strIngredient4 ? <li>{props.drink.strIngredient4 + " = " + props.drink.strMeasure4}</li>:''}
-                     {props.drink.strIngredient5 ? <li>{props.drink.strIngredient5 + " = " + props.drink.strMeasure5}</li>:''}
-                     {props.drink.strIngredient6 ? <li>{props.drink.strIngredient6 + " = " + props.drink.strMeasure6}</li>:''}
-                     {props.drink.strIngredient7 ? <li>{props.drink.strIngredient7 + " = " + props.drink.strMeasure7}</li>:''}
-                     {props.drink.strIngredient8 ? <li>{props.drink.strIngredient8 + " = " + props.drink.strMeasure8}</li>:''}
-                     {props.drink.strIngredient9 ? <li>{props.drink.strIngredient9 + " = " + props.drink.strMeasure9}</li>:''}
-                     {props.drink.strIngredient10 ? <li>{props.drink.strIngredient10 + " = " + props.drink.strMeasure10}</li>:''}
-                     {props.drink.strIngredient11 ? <li>{props.drink.strIngredient11 + " = " + props.drink.strMeasure11}</li>:''}
-                     {props.drink.strIngredient12 ? <li>{props.drink.strIngredient12 + " = " + props.drink.strMeasure12}</li>:''}
-                     {props.drink.strIngredient13 ? <li>{props.drink.strIngredient13 + " = " + props.drink.strMeasure13}</li>:''}
-                     {props.drink.strIngredient14 ? <li>{props.drink.strIngredient14 + " = " + props.drink.strMeasure14}</li>:''}
-                     {props.drink.strIngredient15 ? <li>{props.drink.strIngredient15 + " = " + props.drink.strMeasure15}</li>:''}
+                 {toIngLst(props.drink).map((ingredient) => <li>{ingredient}</li>)}
                  </ul>
              </div>
             </section>
